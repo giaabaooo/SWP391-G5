3daffcc46a1d118d7338cc756c7901fab4e1735c
Gia Bao <giabaobs35@gmail.com>
2025-10-19 22:43:23 +0700

    fix filter status and type

--- CRM/src/java/Controller/customer/ListRequestController.java
+++ CRM/src/java/Controller/customer/ListRequestController.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller.customer;
 
 import dal.CustomerRequestDAO;
+import dal.DeviceDAO;
 import data.CustomerRequest;
 import data.User;
 import java.io.IOException;
@@ -22,13 +22,13 @@ import java.util.List;
  * @author admin
  */
 public class ListRequestController extends HttpServlet {
+
     private CustomerRequestDAO CustomerRequestDAO = new CustomerRequestDAO();
     private final int LIMIT = 10; // số items 1 trang
-    
-    
-   @Override
+    private DeviceDAO deviceDAO = new DeviceDAO();
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("user") == null) {
             response.sendRedirect("../login");
@@ -36,13 +36,13 @@ public class ListRequestController extends HttpServlet {
         }
 
         User user = (User) session.getAttribute("user");
-        
+
         // Lấy param từ URL (nếu có)
         String keyword = request.getParameter("keyword");
         String type = request.getParameter("type");
         String status = request.getParameter("status");
         String pageParam = request.getParameter("page");
-        
+
         // Phân trang
         int page = 1;
         if (pageParam != null) {
@@ -55,19 +55,20 @@ public class ListRequestController extends HttpServlet {
 
         int offset = (page - 1) * LIMIT;
 
-        
         List<CustomerRequest> list = CustomerRequestDAO.getRequestsByUserId(user.getId(), keyword, type, status, offset, LIMIT);
+        List<String> requestTypes = List.of("WARRANTY", "REPAIR");
+        List<String> statuses = List.of("PENDING", "SCHEDULED", "COMPLETED", "OVERDUE");
 
-       
+        request.setAttribute("types", requestTypes);
+        request.setAttribute("statuses", statuses);
+
         request.setAttribute("list", list);
         request.setAttribute("currentPage", page);
         request.setAttribute("keyword", keyword);
         request.setAttribute("type", type);
         request.setAttribute("status", status);
 
-        
         request.getRequestDispatcher("/customer/listRequest.jsp").forward(request, response);
-    } 
+    }
 
-   
 }
